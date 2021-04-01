400875cf16d483e70721a6086095b1c05ab86615
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2021-04-02 01:25:28 +0500

    :sunglasses:

--- color_detection.py
+++ color_detection.py
@@ -1,14 +1,21 @@
 import numpy as np 
 import cv2
-image = cv2.imread("red_shirt.jpg")
+image = cv2.imread("yellow_shirt.jpg")
 boundaries = [
-	([17, 15, 100], [50, 56, 200]),
-	([86, 31, 4], [220, 88, 50]),
-	([25, 146, 190], [62, 174, 250]),
-	([103, 86, 65], [145, 133, 128])
+	([17, 15, 100], [50, 56, 200]),  #red
+	([86, 31, 4], [220, 88, 50]),    #blue
+	([25, 146, 190], [62, 174, 250]), #yellow
+	([103, 86, 65], [145, 133, 128]) #green
 ]
-d = {}
+colors = [
+	"red",
+	"blue",
+	"yellow",
+	"green"
+]
+colors_count = {}
 for (lower, upper) in boundaries:
+	temp_tuple = (lower, upper)
 	# create NumPy arrays from the boundaries
 	lower = np.array(lower, dtype = "uint8")
 	upper = np.array(upper, dtype = "uint8")
@@ -16,18 +23,16 @@ for (lower, upper) in boundaries:
 	# the mask
 	mask = cv2.inRange(image, lower, upper)
 	output = cv2.bitwise_and(image, image, mask = mask)
-	print (output)
+	new = output.flatten().tolist()
+	count = 0
+	for i in new:
+		if i != 0:
+			count += 1
+	#print (temp_tuple)
+	idx = boundaries.index(temp_tuple)
+	clr = colors[idx]
+	colors_count[clr] = count
 	# show the images
-	cv2.imshow("images", np.hstack([image, output]))
+	#cv2.imshow("images", np.hstack([image, output]))
 	cv2.waitKey(0)
-
-
-'''
-intialize a dict before the for loop
-
-for each output image, check how many of the pixels are not black
-
-put the number of pixels that are not black and the color that the output was checking for (like what index it is in boundaries) in a dictionary
-
-after the for loop choose the color with the highest number of pixels that are not black from the dictionary
-'''
+print (max(colors_count, key=colors_count.get))
