b0719592219d0dcf1b9b42b68f15418886beeae8
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2021-04-02 00:56:27 +0500

    :flushed:

--- color_detection.py
+++ color_detection.py
@@ -16,6 +16,7 @@ for (lower, upper) in boundaries:
 	# the mask
 	mask = cv2.inRange(image, lower, upper)
 	output = cv2.bitwise_and(image, image, mask = mask)
+	print (output)
 	# show the images
 	cv2.imshow("images", np.hstack([image, output]))
 	cv2.waitKey(0)
